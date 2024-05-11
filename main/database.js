6a09d871ecb7850301051aa30ad47876c38a29e7
lordumungus <alessandro.costa6@fatec.sp.gov.br>
2024-05-11 19:12:48 -0300

    Adicionando NODE API

---
+++ main/database.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const mysql = require('mysql');
+
+// Crie uma conexão com o banco de dados MySQL
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'acelera2024'
+});
+
+// Mantenha a conexão com o banco de dados aberta
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err);
+    return;
+  }
+  console.log('Conexão com o banco de dados MySQL estabelecida');
+});
+
+// Função para inserir os dados do arquivo JSON no banco de dados
+function inserirDadosDoJson(filename) {
+  // Leia o arquivo JSON
+  fs.readFile(filename, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Erro ao ler o arquivo JSON:', err);
+      return;
+    }
+
+    try {
+      const dados = data.trim().split('\n').map(line => JSON.parse(line));
+
+      const query = 'INSERT INTO leituras_nivel_agua (timestamp, valor) VALUES (?, ?)';
+      
+      dados.forEach((item) => {
+        const values = [item.timestamp, item.value];
+        connection.query(query, values, (error, results, fields) => {
+          if (error) {
+            console.error('Erro ao inserir dados:', error);
+          } else {
+            console.log('Dados inseridos com sucesso!');
+          }
+        });
+      });
+    } catch (error) {
+      console.error('Erro ao analisar o arquivo JSON:', error);
+    }
+  });
+}
+
+module.exports = { inserirDadosDoJson };
---
+++ main/main.js
@@ -0,0 +1,55 @@
+const { SerialPort } = require('serialport');
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const fs = require('fs');
+const { inserirDadosDoJson } = require('./database');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+app.use(express.static('public'));
+app.get('/', (req, res, next) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(3002, () => {
+  console.log('Servidor iniciado na porta 3002');
+});
+
+const readline = require('readline');
+const arduino = new SerialPort({ path: 'COM3', baudRate: 9600 });
+const rl = readline.createInterface({ input: arduino });
+arduino.on('open', () => {
+  console.log('Conexão Serial Iniciada');
+  //teste emitindo grafico
+ 
+  //teste
+});
+
+
+rl.on('line', (line) => {
+  console.log('Recebido:', line);
+  
+  // Objeto para representar os dados recebidos
+  const data = {
+    timestamp: new Date().toISOString(), // Adiciona um carimbo de data/hora
+    value: line.trim() // Remove espaços em branco extras e atribui o valor recebido
+  };
+
+  // Converte o objeto em formato JSON
+  const jsonData = JSON.stringify(data);
+
+  // Nome do arquivo para salvar os dados (pode ser ajustado conforme necessário)
+  const filename = 'dados.json';
+
+  // Adiciona os dados ao arquivo JSON
+  fs.appendFile(filename, jsonData + '\n', (err) => {
+    if (err) {
+      console.error('Erro ao gravar no arquivo:', err);
+    } else {
+      console.log('Dados gravados com sucesso!');
+      // Chame a função para inserir os dados do arquivo JSON no banco de dados
+      inserirDadosDoJson(filename);
+    }
+  });
+});
